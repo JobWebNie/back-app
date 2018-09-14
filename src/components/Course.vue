1c5cbd3c3eec0fb63081c795b8b55f4fd882d618
JobWebNie <1979510089@qq.com>
2018-09-15 01:35:20 +0800

    "添加course"

---
+++ src/components/Course.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="n_course">
+            <div class="course-name course-text">
+                <span>课程信息</span>
+            </div>
+            <div class="course-text">
+                你要去相信，没有到不了的明天
+            </div>
+            <div class="course-article">
+                五十多位连环画大家联手打造，畅销海外中文世界三十年。
+                万余张彩绘、写实精美插图，连缀成五千年纸上电影。符合时代特征的人物对话，真实还原古人生活场景。
+            </div>
+            <div class="course-text" style="color: #ff0000; display: flex;justify-content: space-between">
+                <span style="color: #ff0000;">400积分+50元</span>
+                
+                <span style="color: #75c66d;font-size: .8125rem;">可获得800积分</span>
+            </div>
+            <div class="course-tag">
+                <span style="margin-right: 1.625rem;"> 库存：6547</span>
+                <span> 库存：6547</span>
+            </div>
+            <div class="course-name course-text">
+                <span>详情</span>
+            </div>
+            <div class="course-tag">
+                <span style="margin-right: 1.625rem;">型号：5635896547</span>
+                <span> 规格：5635896547</span>
+            </div>
+            <div class="course-tag">
+                <span> 参数：65451231</span>
+            </div>
+            <div class="course-name course-text">
+                <span>购买须知</span>
+            </div>
+            <div class="course-article">
+                孟庆江，1937年生，浙江温州人。1965年毕业于中央美术学院国画系人物画专业，师从蒋兆和、叶浅予、刘凌沧等绘画名家。擅长工笔画、人物画，作品《蔡文姬》《长恨歌》等多次获得全国连环画创作评奖大奖殊荣，并被中国美术馆收藏。
+                历任人民美术出版社专职画家、《连环画报》主编、《中国艺术》副主编，享受国务院特殊津贴。
+            </div>
+        </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+.n_course{
+    padding: 0 .9375rem;
+    width: 100%;
+    padding-bottom: 4.375rem;
+    box-sizing: border-box;
+}
+.course-text{
+    margin: .9375rem 0;
+    font-size: .9375rem;
+}
+
+.course-name{
+    border-left: .25rem solid #4d8fff;
+    padding-left: .53125rem;
+    color: #565656;
+}
+.desc{
+    color: #222222;
+   
+}
+.course-article{
+    line-height: 1.125rem;
+    font-size: .8125rem;
+    color: #565656;
+}
+
+.course-tag{
+    font-size: .8125rem;
+    color: #bcbcbc
+}
+</style>
